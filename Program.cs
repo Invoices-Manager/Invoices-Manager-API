ee9b3814cee9c5fa2086d32490e3d472d89fb17a
Schecher_1 <Schecher_1@web.de>
2023-03-26 15:55:57 +0200

    Program cs cleanUp

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
+// Create builder.
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
+// Create app (API)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Run app
 app.Run();
