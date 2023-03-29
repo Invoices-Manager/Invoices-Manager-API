50ff85cd9e76b255cad5d6ddd58a71af2c26a53a
Schecher_1 <Schecher_1@web.de>
2023-03-29 17:42:32 +0200

    fixed the werid "CheckIfExpired" bug

--- Security/JWT.cs
+++ Security/JWT.cs
@@ -19,15 +19,14 @@ namespace Invoices_Manager_API.Security
                 new Claim(ClaimTypes.NameIdentifier, user.Username),
                 new Claim(ClaimTypes.Name, user.FirstName),
                 new Claim(ClaimTypes.Surname, user.LastName),
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.Expiration, timeStamp.AddMinutes(Convert.ToDouble(config["JwtKeys:Expiration"])).ToString("yyyy/MM/dd HH:mm:ss")),
+                new Claim(ClaimTypes.Email, user.Email)
             };
 
             var JwtToken = new JwtSecurityToken(
                 config["JwtKeys:Issuer"],
                 config["JwtKeys:Audience"],
                 Claims,
-                expires: DateTime.Now.AddMinutes(Convert.ToDouble(config["JwtKeys:Expiration"])),
+                expires: timeStamp.AddMinutes(Convert.ToDouble(config["JwtKeys:Expiration"])),
                 signingCredentials: Credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(JwtToken);
@@ -48,17 +47,18 @@ namespace Invoices_Manager_API.Security
                     return true;
 
                 //set the "expiredOn" timestamp
-                DateTimeOffset dateTimeOffSet = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(expiry));
-                DateTime expiredOn = dateTimeOffSet.DateTime;
+                long unixTime = Convert.ToInt64(expiry);
+                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTime);
+                DateTime dateTime = dateTimeOffset.UtcDateTime;
 
                 //check if it is already expired
-                if (expiredOn < DateTime.Now)
+                if (dateTime > DateTime.Now)
                     return true;
 
                 //if the expiry is lower than DateTime.Now
                 return false;
             }
-            catch (Exception)
+            catch
             {
                 return true;
             }
