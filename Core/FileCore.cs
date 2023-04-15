3b5c72fc3a7d253b981dcce8d9c709fd8c116cdb
Schecher_1 <Schecher_1@web.de>
2023-04-15 15:57:05 +0200

    Edit Path stucture

--- Core/FileCore.cs
+++ Core/FileCore.cs
@@ -4,11 +4,14 @@ namespace Invoices_Manager_API.Core
 {
     internal class FileCore
     {
-        private const string SYSPATH = @"Data\";
-        internal static void SaveFile(string tempFilePath, UserModel user)
+        private const string SYS_PATH = @"Data\";
+        private static readonly string CACHE_PATH = Path.Combine(SYS_PATH, "Cache");
+        private static readonly string USERDATA_PATH = Path.Combine(SYS_PATH, "Userdata");
+
+        internal static void SaveInvoiceFile(string tempFilePath, UserModel user)
         {
             //get the path
-            string filePath = Path.Combine(SYSPATH, user.Username);
+            string filePath = Path.Combine(USERDATA_PATH, user.Username);
 
             //check if the Path exist
             if (!Directory.Exists(filePath))
