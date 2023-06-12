8633114198278ddc5ef586049da6de25e71b6da3
Schecher_1 <Schecher_1@web.de>
2023-06-12 16:53:29 +0200

    remove the backup part (will be added in v02 anyway)

--- DataBaseContext.cs
+++ DataBaseContext.cs
@@ -6,9 +6,6 @@ namespace Invoices_Manager_API
     {
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
 
-        public DbSet<BackUpInfoModel> BackUpInfo { get; set; }
-        public DbSet<BackUpModel> BackUp { get; set; } 
-        public DbSet<InvoiceBackUpModel> InvoiceBackUp { get; set; } 
         public DbSet<InvoiceModel> Invoice { get; set; }
         public DbSet<NoteModel> Note { get; set; } 
         public DbSet<UserModel> User { get; set; }
